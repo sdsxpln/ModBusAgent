27ace150eb3159b3e1641dbd10b43872ce3c9c7c
jiliang.tan <jiliang.tan@papa.com.cn>
2017-06-02 15:29:04 +0800

    use select socket

--- src/main.c
+++ src/main.c
@@ -9,13 +9,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 #include "iniparser.h"
 
 // define some constants
 #define START_GATEWAY_PORT 8888
 #define START_API_PORT 8801
+#define TRUE    1
+#define FALSE   0
 
 // functions declaration
 int start_socket(int start_port);
@@ -29,11 +37,12 @@ void create_process(void);
  *
  * @return void
  */
-void 
+    int  
 main(int argc, char * argv[])
 {
     dictionary  *   ini;
     char *ini_name;
+    int start_port=8801;
 
     // check the numbers of args
     if (argc!=2) {
@@ -42,7 +51,10 @@ main(int argc, char * argv[])
         ini_name = argv[1];
         ini = iniparser_load(ini_name);     
         iniparser_dump(ini, stderr);
+        printf("starting...................\n");
+        start_socket(start_port);
     }
+    return 0;
 }
 
 
@@ -53,16 +65,168 @@ main(int argc, char * argv[])
  *
  * @return 
  */
-int 
+    int 
 start_socket(int port)
 {
+    int opt = TRUE;
+    int master_socket , addrlen , new_socket , client_socket[30] , max_clients = 30 , activity, i , valread , sd;
+    int max_sd;
+    struct sockaddr_in address;
+
+    char buffer[1025];  //data buffer of 1K 2014+\n
+
+    //set of socket descriptors
+    fd_set readfds;
+
+    //test massage
+    char *message = "hello world\n";
+
+    //initialise all client_socket[] to 0 so not checked
+    for (i = 0; i < max_clients; i++) 
+    {
+        client_socket[i] = 0;
+    }
+
+    //create a master socket
+    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //set master socket to allow multiple connections , this is just a good habit, it will work without this
+    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    //type of socket created
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( port );
+
+    //bind the socket to localhost port 8888
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("gateway api listening on port %d \n", port);
+
+    //try to specify maximum of 3 pending connections for the master socket
+    if (listen(master_socket, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    //accept the incoming connection
+    addrlen = sizeof(address);
+    puts("Waiting for connections ...");
+
+    while(TRUE) 
+    {
+        //clear the socket set
+        FD_ZERO(&readfds);
+
+        //add master socket to set
+        FD_SET(master_socket, &readfds);
+        max_sd = master_socket;
+
+        //add child sockets to set
+        for ( i = 0 ; i < max_clients ; i++) 
+        {
+            //socket descriptor
+            sd = client_socket[i];
+
+            //if valid socket descriptor then add to read list
+            if(sd > 0)
+                FD_SET( sd , &readfds);
+
+            //highest file descriptor number, need it for the select function
+            if(sd > max_sd)
+                max_sd = sd;
+        }
+
+        //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
+        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+
+        if ((activity < 0) && (errno!=EINTR)) 
+        {
+            printf("select error");
+        }
+
+        //If something happened on the master socket , then its an incoming connection
+        if (FD_ISSET(master_socket, &readfds)) 
+        {
+            if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+
+            //inform user of socket number - used in send and receive commands
+            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+            //send new connection greeting message
+            if( (size_t)send(new_socket, message, strlen(message), 0) != strlen(message) ) 
+            {
+                perror("send");
+            }
+
+            puts("Welcome message sent successfully");
+
+            //add new socket to array of sockets
+            for (i = 0; i < max_clients; i++) 
+            {
+                //if position is empty
+                if( client_socket[i] == 0 )
+                {
+                    client_socket[i] = new_socket;
+                    printf("Adding to list of sockets as %d\n" , i);
+
+                    break;
+                }
+            }
+        }
+
+        //else its some IO operation on some other socket :)
+        for (i = 0; i < max_clients; i++) 
+        {
+            sd = client_socket[i];
+
+            if (FD_ISSET( sd , &readfds)) 
+            {
+                //Check if it was for closing , and also read the incoming message
+                if ((valread = read( sd , buffer, 1024)) == 0)
+                {
+                    //Somebody disconnected , get his details and print
+                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
+                    printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+                    //Close the socket and mark as 0 in list for reuse
+                    close( sd );
+                    client_socket[i] = 0;
+                }
+
+                //Echo back the message that came in
+                else
+                {
+                    //set the string terminating NULL byte on the end of the data read
+                    buffer[valread] = '\0';
+                    send(sd , buffer , strlen(buffer) , 0 );
+                }
+            }
+        }
+    }
 
 }
 
 /**
  * @brief create new process
  */
-void
+    void
 create_process(void)
 {
     pid_t pid = fork();
@@ -70,10 +234,10 @@ create_process(void)
     if (pid == 0) {
         // create child process success
 
-    
+
     } else if(pid > 0) {
         // parent process
-    
+
     } else {
         // fork child process error
         fprintf(stderr, "fork child error\n");
